714a0ff191d6b84c3c635de2b6d572a94affae5b
hang <ucabhz9@ucl.ac.uk>
2024-02-19 22:19:44 +0800

    Add logback configuration file and Dependencies Change

--- src/main/java/com/hang/wiki/WikiApplication.java
+++ src/main/java/com/hang/wiki/WikiApplication.java
@@ -1,13 +1,20 @@
 package com.hang.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Start Successfully!");
+        LOG.info("Address: http://localhost:{}", env.getProperty("server.port"));
     }
 
 }
