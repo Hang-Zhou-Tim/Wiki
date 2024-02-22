7e9a2ea351f2c493afe2c0636505091c31ca4287
hang <ucabhz9@ucl.ac.uk>
2024-02-22 21:46:05 +0800

    Design resuable component for footer + header.

---
+++ web/src/components/FooterComponent.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+    name:'HeaderComponent'
+  });
+</script>
+
+<template>
+  <a-layout-footer style="text-align: center">
+    Hangs HandBook
+  </a-layout-footer>
+</template>
+
+<style scoped>
+
+</style>
