f3682054cbd792dc2d3eead48d291f6c82a3025a
hang <ucabhz9@ucl.ac.uk>
2024-02-24 10:38:13 +0800

    Implement Ebook Administration Front-end Table

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,7 +1,137 @@
 <template>
-  <div class="about">
-    <h1>Ebook Administration</h1>
-  </div>
+  <a-layout>
+      <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+      >
+        <!-- Init a new table. Columns rule based on columns, and data provided by ebooks.
+          -->
+      <a-table
+          :columns="columns"
+          :row-key="record => record.id"
+          :data-source="ebooks"
+          :pagination="pagination"
+          :loading="loading"
+          @change="handleTableChange"
+      >
+
+        <!-- Replace slot in column Cover by img.     -->
+        <template #cover="{ text: cover }">
+          <img v-if="cover" :src="cover" alt="avatar" />
+        </template>
+        <!-- Replace slot in column Action by keyword Edit and Delete Button
+             Text represents the value of corresponding data index. Record represent the whole object.
+        -->
+        <template v-slot:action="{ text, record }">
+          <a-space size="small">
+            <a-button type="primary">
+              Edit
+            </a-button>
+            <a-button type="danger">
+              Delete
+            </a-button>
+          </a-space>
+        </template>
+      </a-table>
+    </a-layout-content>
+  </a-layout>
 </template>
-<script setup lang="ts">
+
+<script lang="ts">
+import {defineComponent, onMounted, ref} from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'AdminEbook',
+  setup() {
+    const ebooks = ref();
+    const pagination = ref({
+      current: 1,
+      pageSize: 10,
+      total: 0
+    });
+    const loading = ref(false);
+    //Define column rules here
+    const columns = [
+      {
+        title: 'cover',
+        dataIndex: 'cover',
+        slots: { customRender: 'cover' }   //All data in this column except title will be rendered by slot rule above.
+      },
+      {
+        title: 'name',
+        dataIndex: 'name'
+      },
+      {
+        title: 'category1',
+        dataIndex: 'category1Id'
+      },
+      {
+        title: 'category2',
+        dataIndex: 'category2Id'
+      },
+      {
+        title: 'docCount',
+        dataIndex: 'docCount'
+      },
+      {
+        title: 'viewCount',
+        dataIndex: 'viewCount'
+      },
+      {
+        title: 'voteCount',
+        dataIndex: 'voteCount'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        slots: { customRender: 'action' }
+      }
+    ];
+
+    /**
+     * Query ebooks
+     **/
+    const handleQuery = (params: any) => {
+      loading.value = true;
+      axios.get("/ebook/list",params).then((response) => {
+        loading.value = false;
+        const data = response.data;
+        ebooks.value = data.content;
+
+        // Reset Pagination Button
+        pagination.value.current = params.page;
+      });
+    };
+
+    /**
+     * Click on Pagination
+     */
+    const handleTableChange = (pagination: any) => {
+      console.log("Check Pagination: " + pagination);
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    };
+
+    onMounted(() => {
+      handleQuery({});
+    });
+
+    return {
+      ebooks,
+      pagination,
+      columns,
+      loading,
+      handleTableChange
+    }
+  }
+});
 </script>
+
+<style scoped>
+img {
+  width: 50px;
+  height: 50px;
+}
+</style>
