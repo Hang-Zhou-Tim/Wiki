7ea91c8bba5710eb1dd8da4c5769e00815be7938
hang <ucabhz9@ucl.ac.uk>
2024-02-26 11:59:41 +0800

    Generate Dao Layer Code in Backend

--- doc/all.sql
+++ doc/all.sql
@@ -25,45 +25,65 @@ insert into `demo` (id, name) values (1,'test');
 drop table if exists `ebook`;
 create table `ebook` (
                          `id` bigint not null comment 'id',
-                         `name` varchar(50) comment '名称',
-                         `category1_id` bigint comment '分类1',
-                         `category2_id` bigint comment '分类2',
-                         `description` varchar(200) comment '描述',
-                         `cover` varchar(200) comment '封面',
-                         `doc_count` int not null default 0 comment '文档数',
-                         `view_count` int not null default 0 comment '阅读数',
-                         `vote_count` int not null default 0 comment '点赞数',
+                         `name` varchar(50) comment 'name',
+                         `category1_id` bigint comment 'category 1',
+                         `category2_id` bigint comment 'category 2',
+                         `description` varchar(200) comment 'description',
+                         `cover` varchar(200) comment 'cover',
+                         `doc_count` int not null default 0 comment 'doc count',
+                         `view_count` int not null default 0 comment 'view count',
+                         `vote_count` int not null default 0 comment 'vote count',
                          primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='电子书';
+) engine=innodb default charset=utf8mb4 comment='Ebook';
 
-insert into `ebook` (id, name, description) values (1, 'Spring Boot 入门教程', '零基础入门 Java 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, description) values (2, 'Vue 入门教程', '零基础入门 Vue 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, description) values (3, 'Python 入门教程', '零基础入门 Python 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, description) values (4, 'Mysql 入门教程', '零基础入门 Mysql 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, description) values (5, 'Oracle 入门教程', '零基础入门 Oracle 开发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description) values (1, 'Spring Boot Beginner Module', 'Java Development for Beginner，First-Choice Framework in Industrial-Level Application');
+insert into `ebook` (id, name, description) values (2, 'Vue Module Beginner Module', 'Vue Development for Beginner，First-Choice Framework in Industrial-Level Application');
+insert into `ebook` (id, name, description) values (3, 'Python Beginner Module', 'Python Development for Beginner，First-Choice Framework in Industrial-Level Application');
+insert into `ebook` (id, name, description) values (4, 'Mysql Beginner Module', 'MySql Development for Beginner，First-Choice Framework in Industrial-Level Application');
+insert into `ebook` (id, name, description) values (5, 'Oracle Beginner Module', 'Oracle Development for Beginner，First-Choice Framework in Industrial-Level Application');
 
 
 drop table if exists `category`;
 create table `category` (
                             `id` bigint not null comment 'id',
-                            `parent` bigint not null default 0 comment '父id',
-                            `name` varchar(50) not null comment '名称',
-                            `sort` int comment '顺序',
+                            `parent` bigint not null default 0 comment 'parent id',
+                            `name` varchar(50) not null comment 'name',
+                            `sort` int comment 'sort',
                             primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='分类';
+) engine=innodb default charset=utf8mb4 comment='Category';
 
-insert into `category` (id, parent, name, sort) values (100, 000, '前端开发', 100);
+insert into `category` (id, parent, name, sort) values (100, 000, 'Front-End Development', 100);
 insert into `category` (id, parent, name, sort) values (101, 100, 'Vue', 101);
 insert into `category` (id, parent, name, sort) values (102, 100, 'HTML & CSS', 102);
 insert into `category` (id, parent, name, sort) values (200, 000, 'Java', 200);
-insert into `category` (id, parent, name, sort) values (201, 200, '基础应用', 201);
-insert into `category` (id, parent, name, sort) values (202, 200, '框架应用', 202);
+insert into `category` (id, parent, name, sort) values (201, 200, 'Basic App', 201);
+insert into `category` (id, parent, name, sort) values (202, 200, 'Framework APP', 202);
 insert into `category` (id, parent, name, sort) values (300, 000, 'Python', 300);
-insert into `category` (id, parent, name, sort) values (301, 300, '基础应用', 301);
-insert into `category` (id, parent, name, sort) values (302, 300, '进阶方向应用', 302);
-insert into `category` (id, parent, name, sort) values (400, 000, '数据库', 400);
+insert into `category` (id, parent, name, sort) values (301, 300, 'Basic App', 301);
+insert into `category` (id, parent, name, sort) values (302, 300, 'Advanced App', 302);
+insert into `category` (id, parent, name, sort) values (400, 000, 'Database', 400);
 insert into `category` (id, parent, name, sort) values (401, 400, 'MySQL', 401);
-insert into `category` (id, parent, name, sort) values (500, 000, '其它', 500);
-insert into `category` (id, parent, name, sort) values (501, 500, '服务器', 501);
-insert into `category` (id, parent, name, sort) values (502, 500, '开发工具', 502);
-insert into `category` (id, parent, name, sort) values (503, 500, '热门服务端语言', 503);
+insert into `category` (id, parent, name, sort) values (500, 000, 'Others', 500);
+insert into `category` (id, parent, name, sort) values (501, 500, 'Server', 501);
+insert into `category` (id, parent, name, sort) values (502, 500, 'Dev Tool', 502);
+insert into `category` (id, parent, name, sort) values (503, 500, 'Top Language', 503);
+
+-- 文档表
+drop table if exists `doc`;
+create table `doc` (
+                       `id` bigint not null comment 'id',
+                       `ebook_id` bigint not null default 0 comment 'Ebook id',
+                       `parent` bigint not null default 0 comment 'Parent id',
+                       `name` varchar(50) not null comment 'Name',
+                       `sort` int comment 'Sequence',
+                       `view_count` int default 0 comment 'View Count',
+                       `vote_count` int default 0 comment 'Vote Count',
+                       primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='Doc Table';
+
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (1, 1, 0, 'Sample 1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (2, 1, 1, 'Sample 1.1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (3, 1, 0, 'Sample 2', 2, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (4, 1, 3, 'Sample 2.1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (5, 1, 3, 'Sample 2.2', 2, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (6, 1, 5, 'Sample 2.2.1', 1, 0, 0);
