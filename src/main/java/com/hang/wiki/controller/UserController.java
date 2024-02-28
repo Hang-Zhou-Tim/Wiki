3ce9a8c3bd0cf9647196ceee8af2a85fb1a74f3d
hang <ucabhz9@ucl.ac.uk>
2024-02-28 14:02:18 +0800

    Store user details as global variable in Session Storage

--- src/main/java/com/hang/wiki/controller/UserController.java
+++ src/main/java/com/hang/wiki/controller/UserController.java
@@ -9,17 +9,29 @@ import com.hang.wiki.resp.PageResp;
 import com.hang.wiki.resp.UserLoginResp;
 import com.hang.wiki.resp.UserQueryResp;
 import com.hang.wiki.service.UserService;
+import com.hang.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private SnowFlake snowFlake;
 
     @GetMapping("/list")
     public CommonResp<PageResp<UserQueryResp>> list(@Valid UserQueryReq userQueryReq){
@@ -47,10 +59,15 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public CommonResp<UserLoginResp> resetPassword(@Valid @RequestBody UserLoginReq req){
+    public CommonResp<UserLoginResp> login(@Valid @RequestBody UserLoginReq req){
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        LOG.info("Generate and Put Login token in Redis.");
+        Long token = snowFlake.nextId();
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token,userLoginResp,3600*24, TimeUnit.SECONDS);
         resp.setContent(userLoginResp);
         return resp;
     }
