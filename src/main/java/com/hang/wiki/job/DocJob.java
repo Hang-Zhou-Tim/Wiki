f6b5ad56546a74d29ab198566bca6e3c7fd9f3e7
hang <ucabhz9@ucl.ac.uk>
2024-02-29 17:35:03 +0800

    Update Vote and View Count Every 5 Seconds

---
+++ src/main/java/com/hang/wiki/job/DocJob.java
@@ -0,0 +1,37 @@
+package com.hang.wiki.job;
+
+import com.hang.wiki.service.DocService;
+import com.hang.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class DocJob {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
+
+    @Resource
+    private DocService docService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    /**
+     * Update Ebook info every 5 min
+     */
+    @Scheduled(cron = "*/5 * * * * ?")
+    public void cron() {
+        // Add Log Stream ID
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
+
+        long start = System.currentTimeMillis();
+        docService.updateEbookInfo();
+        LOG.info("Ebook Info Updated within {} ms.", System.currentTimeMillis() - start);
+    }
+
+}
