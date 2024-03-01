5c2131f3178d8cbf6b58d6e0d5bc3c00ab7c374f
hang <ucabhz9@ucl.ac.uk>
2024-03-01 14:37:56 +0800

    Display echart for view/vote count increment for past 30 days. Fix minor bugs.

--- web/src/App.vue
+++ web/src/App.vue
@@ -30,7 +30,7 @@ export default defineComponent({
   background: rgba(255, 255, 255, 0.3);
 }
 
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+.ant-row-rtl #components-layout-demo-top-side-2 {
   float: right;
   margin: 16px 0 16px 24px;
 }
